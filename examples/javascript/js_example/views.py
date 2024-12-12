b9456f19b11b4c2ed6326748ae986f08fdc5e315
chris <Mileschris466@gmail.com>
2024-12-12 10:11:54 +0300

    changes

--- examples/javascript/js_example/views.py
+++ examples/javascript/js_example/views.py
@@ -1,6 +1,6 @@
-from flask import jsonify
-from flask import render_template
-from flask import request
+from flask import jsonify;
+from flask import render_template;
+from flask import request;
 
 from . import app
 
